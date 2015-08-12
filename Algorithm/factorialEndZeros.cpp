3d0118feee2f17b7f9fd43ce957ecf39f54823f7
yqwang <1217172817@qq.com>
2015-08-12 10:57:54 +0800

    Update factorialEndZeros.cpp

--- Algorithm/factorialEndZeros.cpp
+++ Algorithm/factorialEndZeros.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 //计算n的阶乘末尾零的个数
+//统计1~n数每个数的因式分解中5的指数，即该数一定是能被5整除的，然后求和即可
 int factorialEndZeros1(int n)
 {
 	int nums = 0;
@@ -15,6 +16,7 @@ int factorialEndZeros1(int n)
 	}
 	return nums;
 }
+//Z={[N/5] + [N/5^2]+[N/5^3] + ...},5^k > N 时结束，[N/5^k] =0
 int factorialEndZeros2(int n)
 {
 	int nums = 0;
